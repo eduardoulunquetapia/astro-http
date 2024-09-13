2c4f3a658515ba6285a77ccdec15958632e275aa
Eduardo Ulunque Tapia <eduardo.ulunque@kunn.es>
2024-09-13 04:35:26 +0200

    feat: Add Cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,17 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
-import node from '@astrojs/node';
+/* import node from '@astrojs/node'; */
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap()],
-  output: 'hybrid',
-
+  output: 'server',
+  adapter: cloudflare()
+  /* output: 'hybrid',
   adapter: node({
     mode: 'standalone',
-  }),
+  }), */
 });
